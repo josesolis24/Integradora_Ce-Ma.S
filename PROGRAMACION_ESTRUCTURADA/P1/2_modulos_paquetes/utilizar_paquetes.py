cc727f90f1cfcde027d629fa6a53580451eb5465
josesolis24 <jsolisemiliano24@gmail.com>
2025-11-25 12:10:28 -0600

    Delete PROGRAMACION_ESTRUCTURADA directory

--- PROGRAMACION_ESTRUCTURADA/P1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras Ruano"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\n\t Agenda telefonia \n\t\ Nombre: {nom}")
-
